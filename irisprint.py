3dbcb2482a50bbff023a824ab75ee82ee6ee9c3c
healyshane <36333022+healyshane@users.noreply.github.com>
2018-03-03 18:05:38 +0000

    Add files via upload

---
+++ irisprint.py
@@ -0,0 +1,9 @@
+#Shane Healy 03-MAR-2018
+#Data from http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
+#Exercise 5: Read Iris data set and print rows formatted
+
+with open('data/iris.csv') as f:
+    
+    for line in f:
+        x = line.split(",") # splitting data
+        print('{} {} {} {}'.format(x[0], x[1], x[2], x[3]))
